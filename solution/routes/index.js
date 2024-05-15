57670d5014527d4091f064277b33cb8d5eadfefe
Khanh Linh Tran Lara <tklinh1072002@gmail.com>
2024-05-15 16:11:01 +0100

    working on knowledge graph, but not right

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -41,7 +41,56 @@ router.post('/add', upload.single('plantImg'), function (req, res) {
 	let filePath = req.file.path;
 	let result = plants.create(userData, filePath);
 	console.log(result);
-	res.redirect('/');
+
+    // retrieve plant info from DBPedia
+    const plant_name = "Rose";
+    const resource = `http://dbpedia.org/resource/${plant_name}`;
+    const endpointUrl = 'https://dbpedia.org/sparql';
+
+    const sparqlQuery = `
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX dbo: <http://dbpedia.org/ontology/>
+      PREFIX dbp:<http://dbpedia.org/property/>
+      
+      SELECT ?name ?sci_name ?abstract ?uri
+      WHERE {
+      <${resource}> rdfs:label ?name .
+      OPTIONAL {<${resource}> dbp:taxon ?sci_name .}
+      <${resource}> dbo:abstract ?abstract .
+      BIND (<${resource}> AS ?uri)
+      FILTER (LANG(?abstract) = "en") .
+      FILTER (LANG(?name) = "en") .
+      FILTER (LANG(?sci_name) = "en") .
+  }`;
+
+    const encodedQuery = encodeURIComponent(sparqlQuery);
+
+    const url = `${endpointUrl}?query=${encodedQuery}&format=json`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let bindings = data.results.bindings;
+            if (bindings.length > 0) {
+                res.render('index', {
+                    plant_name:bindings[0].name.value,
+                    plant_sci_name:bindings[0].sci_name ? bindings[0].sci_name.value : "Unknown",
+                    dbpedia_desc: bindings[0].abstract.value,
+                    dbpedia_uri: bindings[0].uri.value
+                    // JSONresult: result
+                });
+            } else {
+                res.render('index', {
+                    plant_name: "Unknown",
+                    plant_sci_name: "Unknown",
+                    dbpedia_desc: "Unknown",
+                    dbpedia_uri: "Unknown"
+                });
+            }
+
+        });
+
+    res.redirect('/');
 });
 
 module.exports = router;
