d0fc38563634b0b6851d57ddb2178deebb365ba2
HenryWCable <henrywcable@gmail.com>
2024-05-16 14:15:13 +0100

    filter working

--- solution/public/javascripts/dialog.js
+++ solution/public/javascripts/dialog.js
@@ -1,39 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const blocks = document.querySelectorAll(".block");
-
-  blocks.forEach(block => {
+function attachDialogEvents() {
+    const blocks = document.querySelectorAll(".block");
+  
+    blocks.forEach(block => {
       block.addEventListener("click", () => {
-          const dialogId = block.dataset.dialogId;
-          const dialog = document.getElementById(dialogId);
-          if (dialog) {
-            dialog.style.display = "flex";
-            setTimeout(() => {
-              dialog.classList.add("opening");
-              dialog.showModal();
-          }, 10); 
-          }
-      });
-  });
-
-  const closeButton = document.querySelectorAll("dialog .close");
-
-  closeButton.forEach(button => {
-      button.addEventListener("click", (event) => {
-          const dialog = event.target.closest("dialog");
-          dialog.classList.remove("opening");
-          dialog.classList.add("closing");
+        const dialogId = block.dataset.dialogId;
+        const dialog = document.getElementById(dialogId);
+        if (dialog) {
+          dialog.style.display = "flex";
           setTimeout(() => {
-            dialog.classList.remove("closing");
-            dialog.close();
-            dialog.style.display = "none";
+            dialog.classList.add("opening");
+            dialog.showModal();
+          }, 10); 
+        }
+      });
+    });
+  
+    const closeButton = document.querySelectorAll("dialog .close");
+  
+    closeButton.forEach(button => {
+      button.addEventListener("click", (event) => {
+        const dialog = event.target.closest("dialog");
+        dialog.classList.remove("opening");
+        dialog.classList.add("closing");
+        setTimeout(() => {
+          dialog.classList.remove("closing");
+          dialog.close();
+          dialog.style.display = "none";
         }, 300);
       });
-  });
-
-  document.querySelectorAll("dialog").forEach(dialog => {
+    });
+  
+    document.querySelectorAll("dialog").forEach(dialog => {
       dialog.addEventListener("close", () => {
-          dialog.classList.remove("opening");
-          dialog.style.display = "none";
+        dialog.classList.remove("opening");
+        dialog.style.display = "none";
       });
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    attachDialogEvents();
   });
-});
+  
