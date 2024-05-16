abcf33f87712771331890310d6558409410760f7
Bravewave <bravewave42@gmail.com>
2024-05-16 02:21:51 +0100

    Create plant details dialog box

---
+++ solution/public/javascripts/dialog.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const blocks = document.querySelectorAll(".block");
+  
+    blocks.forEach(block => {
+      block.addEventListener("click", () => {
+        const dialogId = block.dataset.dialogId;
+        const dialog = document.getElementById(dialogId);
+        if (dialog) {
+          dialog.showModal();
+          dialog.classList.add("opening");
+        }
+      });
+    });
+  
+    const closeButtons = document.querySelectorAll("dialog .close");
+    
+    closeButtons.forEach(button => {
+      button.addEventListener("click", (event) => {
+        const dialog = event.target.closest("dialog");
+        dialog.classList.remove("opening");
+        dialog.close();
+      });
+    });
+  
+    document.querySelectorAll("dialog").forEach(dialog => {
+      dialog.addEventListener("close", () => {
+        dialog.classList.remove("opening");
+      });
+    });
+  });
