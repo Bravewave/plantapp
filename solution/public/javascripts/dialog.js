cd795f34648e4f43fd96278b99e8bb510dd7622c
Bravewave <bravewave42@gmail.com>
2024-05-16 12:31:30 +0100

    Fix dialog blocking page content

--- solution/public/javascripts/dialog.js
+++ solution/public/javascripts/dialog.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const blocks = document.querySelectorAll(".block");
-  
-    blocks.forEach(block => {
+  const blocks = document.querySelectorAll(".block");
+
+  blocks.forEach(block => {
       block.addEventListener("click", () => {
-        const dialogId = block.dataset.dialogId;
-        const dialog = document.getElementById(dialogId);
-        if (dialog) {
-          dialog.showModal();
-          dialog.classList.add("opening");
-        }
+          const dialogId = block.dataset.dialogId;
+          const dialog = document.getElementById(dialogId);
+          if (dialog) {
+              dialog.classList.add("opening");
+              dialog.showModal();
+          }
       });
-    });
-  
-    const closeButtons = document.querySelectorAll("dialog .close");
-    
-    closeButtons.forEach(button => {
-      button.addEventListener("click", (event) => {
-        const dialog = event.target.closest("dialog");
-        dialog.classList.remove("opening");
-        dialog.close();
-      });
-    });
-  
-    document.querySelectorAll("dialog").forEach(dialog => {
-      dialog.addEventListener("close", () => {
-        dialog.classList.remove("opening");
-      });
-    });
   });
+
+  const closeButtons = document.querySelectorAll("dialog .close");
+
+  closeButtons.forEach(button => {
+      button.addEventListener("click", (event) => {
+          const dialog = event.target.closest("dialog");
+          dialog.classList.remove("opening");
+          dialog.close();
+      });
+  });
+
+  document.querySelectorAll("dialog").forEach(dialog => {
+      dialog.addEventListener("close", () => {
+          dialog.classList.remove("opening");
+      });
+  });
+});
